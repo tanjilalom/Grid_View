cef523b358c45c6421c6455d1b03499edbc4af31
tanjilalom <tanjil15-3079@diu.edu.bd>
2024-02-27 15:56:54 +0600

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SafeArea(
+        child: Scaffold(
+          body: Container(
+            child: GridView.builder(
+                itemCount: 10,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2),
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(1.0),
+                    child: Container(
+                      color: Colors.lightBlueAccent,
+                    ),
+                  );
+                }),
+          ),
+        ),
+      ),
+    );
+  }
+}
